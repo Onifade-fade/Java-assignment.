cc0c8b104c79b748d77240f389abb5ed04ff3d2b
Onifade-fade <onifadedamilare4real@gmail.com>
2025-01-25 18:27:34 +0100

    Update NestedLoopFlag.java

--- NestedLoopFlag.java
+++ NestedLoopFlag.java
@@ -1,9 +1,8 @@
 // 1b
 public class NigeriaFlagNestedLoop {
     public static void main(String[] args) {
-        int height = 7; // Number of rows
-        int width = 15; // Total width of the flag
-
+        int height = 7; 
+        int width = 15; 
         for (int i = 1; i <= height; i++) {
             for (int j = 1; j <= width; j++) {
                 if (j <= width / 3 || j > 2 * (width / 3)) {
