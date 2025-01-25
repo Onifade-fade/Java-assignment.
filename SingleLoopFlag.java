f1e24600b51e0c895932a0828e75a7503c7be0f3
Onifade-fade <onifadedamilare4real@gmail.com>
2025-01-25 18:25:57 +0100

    Update SingleLoopFlag.java

--- SingleLoopFlag.java
+++ SingleLoopFlag.java
@@ -1,17 +1,15 @@
 // 1a
 public class NigeriaFlagSingleLoop {
     public static void main(String[] args) {
-        int height = 7; // Number of rows
-        int width = 15; // Total width of the flag
-
+        int height = 7; 
+        int width = 15; 
         for (int i = 1; i <= height; i++) {
             StringBuilder row = new StringBuilder();
-
             for (int j = 1; j <= width; j++) {
                 if (j <= width / 3 || j > 2 * (width / 3)) {
-                    row.append("G"); // Green part
+                    row.append("G"); 
                 } else {
-                    row.append("W"); // White part
+                    row.append("W"); 
                 }
             }
             System.out.println(row.toString());
