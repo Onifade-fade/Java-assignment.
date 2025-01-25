c6b65183bf59f3993481bce3c852a300c8382f97
Onifade-fade <onifadedamilare4real@gmail.com>
2025-01-25 18:26:58 +0100

    Update NigeriaFlagNestedLoop.java

--- NigeriaFlagNestedLoop.java
+++ NigeriaFlagNestedLoop.java
@@ -1,10 +1,8 @@
 // 2b
 public class NigeriaFlagNestedLoop {
     public static void main(String[] args) {
-        int rows = 7;  // Number of rows
-        int columns = 15;  // Total width of the flag
-
-        // Using nested loops
+        int rows = 7;  
+        int columns = 15;  
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 if (j < columns / 3 || j >= 2 * (columns / 3)) {
