eede3dab05b8057db1cf25ed799a760a1482ed1f
Onifade-fade <onifadedamilare4real@gmail.com>
2025-01-25 18:24:11 +0100

    Update UserInput2DArray.java

--- UserInput2DArray.java
+++ UserInput2DArray.java
@@ -6,25 +6,20 @@ public class TwoDArrayInputOutput {
         // Declare a 2D array of size 10x10
         int[][] array = new int[10][10];
         Scanner scanner = new Scanner(System.in);
-
-        // Part a: Assign elements to the 2D array using user input
         System.out.println("Enter values for a 10x10 array:");
-        for (int i = 0; i < array.length; i++) { // Outer loop for rows
-            for (int j = 0; j < array[i].length; j++) { // Inner loop for columns
+        for (int i = 0; i < array.length; i++) { 
+            for (int j = 0; j < array[i].length; j++) { 
                 System.out.print("Enter value for index [" + i + "][" + j + "]: ");
                 array[i][j] = scanner.nextInt();
             }
         }
-
-        // Part b: Print out the 2D array using a for-each loop
         System.out.println("\nYou entered the following values:");
-        for (int[] row : array) { // Iterate over rows
-            for (int value : row) { // Iterate over values in each row
+        for (int[] row : array) { 
+            for (int value : row) { 
                 System.out.print(value + " ");
             }
-            System.out.println(); // Move to the next line after printing a row
+            System.out.println(); 
         }
-
         scanner.close();
     }
 }
